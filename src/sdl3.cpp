2a29b327efca086f716d612ba1d4529c0023f260
Matthew Geiger <matt.g@castus.tv>
2024-11-04 08:48:28 -0800

    Added CMake files

---
+++ src/sdl3.cpp
@@ -0,0 +1,8 @@
+/**
+ * ether2d
+ *
+ * Matthew Todd Geiger
+ * Nov 3 2024
+ *
+ * sdl3.cpp
+ */
---
+++ src/sdl3.hpp
@@ -0,0 +1,14 @@
+/**
+ * ether2d
+ *
+ * Matthew Todd Geiger
+ * Nov 3 2024
+ *
+ * sdl3.hpp
+ */
+
+// For documentation on how this works
+// https://wiki.libsdl.org/SDL3/README/main-functions
+
+#pragma once
+
